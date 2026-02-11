47f759d7568ca5e44e173cce29120e330ecada91
abdullahmard2 <abdullahmard2@gmail.com>
2026-02-11 14:41:15 +0100

    updated the Prohject links

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,30 +17,35 @@ const projectsData = [
     title: "Windmill",
     description: "A 3D project made with Cinema 4D of the windmill Meyers Mühle, Papenburg.",
      imageSrc: require("../images/Windmill.jpg"),
+     liveUrl: "https://architecture-project-viewer.vercel.app/project/3/"
   },
   {
     id: "community-hall",
-    title: "Community Hall for Students",
+    title: "Transit",
     description: "The community hall serves as a kind of interim station for students. Here, they are given the opportunity to use the time between daily activities",
     imageSrc: require("../images/BA_Abgabe.png"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/1/"
   },
   {
     id: "residential-building",
-    title: "Residential Building in the City",
+    title: "Residential Building",
     description: "This residential building was designed as a block in the heart of the city to meet the needs of various types of families, while also addressing urban design requirements.",
     imageSrc: require("../images/Städtebau.jpg"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/2/"
   },
   {
     id: "donut",
     title: "Donut",
     description: "A classic beginner project in Blender, this 3D donut model demonstrates fundamental skills in modeling, texturing, and lighting.It demonstrates the ability to create simple yet visually impactful compositions.",
     imageSrc: require("../images/Donut.jpg"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/4/"
   },
   {
     id: "monastery",
     title: "Monastery in the City",
     description: "Tailored to the needs of the nuns, this monastery was built in the heart of the city to meet their daily requirements, ensuring they don't have to leave frequently unless necessary.",
     imageSrc: require("../images/kloster.png"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/5/"
   },
   {
     id: "forest",
@@ -53,6 +58,7 @@ const projectsData = [
     title: "Forest-2",
     description: "A small Twinmotion project showcasing the exploration of shadows and colors, focusing on optimizing render times for efficient photo-realistic rendering.",
     imageSrc: require("../images/Forest-2.png"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/6/"
   },
 
 ];export default projectsData;
