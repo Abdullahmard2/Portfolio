d35ed8f24a653a94e8ebe9b12a3189c7e78e4b78
Abdullah Almardoud <abdullahmard2@gmail.com>
2026-08-09 20:59:06 +0200

    Fix fetch URL and update headers in useSubmit

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -6,16 +6,19 @@ const useSubmit = () => {
   const submit = async (data) => {
     setLoading(true);
     try {
-      const response = await fetch("https://formspree.io/f/https://formspree.io/f/xeajaajp", {
-        method: "POST",
-        headers: { "Accept": "application/json" },
-        body: JSON.stringify({
-          name: data.firstName,
-          email: data.email,
-          type: data.type,
-          message: data.comment,
-        }),
-      });
+     const response = await fetch("https://formspree.io/f/xeajaajp", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    name: data.firstName,
+    email: data.email,
+    type: data.type,
+    message: data.comment,
+  }),
+});
 
       if (response.ok) {
         return { type: "success", message: `Thanks for your submission ${data.firstName}, we will get back to you shortly!` };
