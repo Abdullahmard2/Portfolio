4cce09311d14218272468aad804073ae5aa7ac48
Abdullah Almardoud <abdullahmard2@gmail.com>
2026-08-09 21:12:15 +0200

    Reorder project highlights

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,17 +1,17 @@
 const projectsData = [
   {
-    id: "windmill",
-    title: "Windmill",
-    description: "A 3D project made with Cinema 4D of the windmill Meyers Mühle, Papenburg.",
-     imageSrc: require("../images/Windmill.jpg"),
-     liveUrl: "https://architecture-project-viewer.vercel.app/project/3/"
-  },
-  {
     id: "community-hall",
     title: "Transit",
     description: "The community hall serves as a kind of interim station for students. Here, they are given the opportunity to use the time between daily activities",
     imageSrc: require("../images/BA_Abgabe.png"),
     liveUrl: "https://architecture-project-viewer.vercel.app/project/1/"
+  },
+  {
+    id: "residential-building",
+    title: "Residential Building",
+    description: "This residential building was designed as a block in the heart of the city to meet the needs of various types of families, while also addressing urban design requirements.",
+    imageSrc: require("../images/Städtebau.jpg"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/2/"
   },
   {id: "Little_Lemon",
     title: "Little Lemon",
@@ -27,11 +27,18 @@ const projectsData = [
      liveUrl: "https://m-g-jewellery.vercel.app/"
  },
   {
-    id: "residential-building",
-    title: "Residential Building",
-    description: "This residential building was designed as a block in the heart of the city to meet the needs of various types of families, while also addressing urban design requirements.",
-    imageSrc: require("../images/Städtebau.jpg"),
-    liveUrl: "https://architecture-project-viewer.vercel.app/project/2/"
+    id: "monastery",
+    title: "Monastery in the City",
+    description: "Tailored to the needs of the nuns, this monastery was built in the heart of the city to meet their daily requirements, ensuring they don't have to leave frequently unless necessary.",
+    imageSrc: require("../images/kloster.png"),
+    liveUrl: "https://architecture-project-viewer.vercel.app/project/5/"
+  },
+   {
+    id: "windmill",
+    title: "Windmill",
+    description: "A 3D project made with Cinema 4D of the windmill Meyers Mühle, Papenburg.",
+     imageSrc: require("../images/Windmill.jpg"),
+     liveUrl: "https://architecture-project-viewer.vercel.app/project/3/"
   },
   {
     id: "donut",
@@ -41,24 +48,18 @@ const projectsData = [
     liveUrl: "https://architecture-project-viewer.vercel.app/project/4/"
   },
   {
-    id: "monastery",
-    title: "Monastery in the City",
-    description: "Tailored to the needs of the nuns, this monastery was built in the heart of the city to meet their daily requirements, ensuring they don't have to leave frequently unless necessary.",
-    imageSrc: require("../images/kloster.png"),
-    liveUrl: "https://architecture-project-viewer.vercel.app/project/5/"
-  },
-  {
     id: "forest",
     title: "Forest",
     description: "A simple Blender project featuring a cozy yet eerie atmosphere of a forest at night.",
     imageSrc: require("../images/Forest.png"),
   },
-{
+  {
     id: "forest-2",
     title: "Forest-2",
     description: "A small Twinmotion project showcasing the exploration of shadows and colors, focusing on optimizing render times for efficient photo-realistic rendering.",
     imageSrc: require("../images/Forest-2.png"),
     liveUrl: "https://architecture-project-viewer.vercel.app/project/6/"
   },
+ 
 
 ];export default projectsData;
